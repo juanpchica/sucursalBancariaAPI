6c95d4a3bd7ed38cff1f702e307f2daf023736d1
juanpchcia <juanpchica@hotmail.com>
2019-02-17 18:09:27 -0500

    Proyecto inicial

---
+++ src/main/java/com/sucursalBancaria/sucursalBancaria/SucursalBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.sucursalBancaria.sucursalBancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucursalBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucursalBancariaApplication.class, args);
+	}
+
+}
